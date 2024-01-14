b5ef88a7af5e3bb280c29ccaac3cd270292a6002
miggow_16 <0306201552@caothang.edu.vn>
2024-01-14 20:06:20 +0700

    fix

--- resources/views/fe/navbar.blade.php
+++ resources/views/fe/navbar.blade.php
@@ -1,6 +1,48 @@
 <header class="ec-header">
     <!--Ec Header Top Start -->
-
+    <div class="header-top">
+        <div class="container">
+            <div class="row align-items-center">
+                <!-- Header Top responsive Action -->
+                <div class="col d-lg-none ">
+                    <div class="ec-header-bottons">
+                        <!-- Header User Start -->
+                        <div class="ec-header-user dropdown">
+                            <button class="dropdown-toggle" data-bs-toggle="dropdown"><i
+                                    class="fi-rr-user"></i></button>
+                            <ul class="dropdown-menu dropdown-menu-right">
+                                <li><a class="dropdown-item" href="register.html">Register</a></li>
+                                <li><a class="dropdown-item" href="checkout.html">Checkout</a></li>
+                                <li><a class="dropdown-item" href="login.html">Login</a></li>
+                            </ul>
+                        </div>
+                        <!-- Header User End -->
+                        <!-- Header Cart Start -->
+                        <a href="wishlist.html" class="ec-header-btn ec-header-wishlist">
+                            <div class="header-icon"><i class="fi-rr-heart"></i></div>
+                            <span class="ec-header-count">4</span>
+                        </a>
+                        <!-- Header Cart End -->
+                        <!-- Header Cart Start -->
+                        <a href="#ec-side-cart" class="ec-header-btn ec-side-toggle">
+                            <div class="header-icon"><i class="fi-rr-shopping-bag"></i></div>
+                            <span class="ec-header-count cart-count-lable">3</span>
+                        </a>
+                        <!-- Header Cart End -->
+                        <a href="javascript:void(0)" class="ec-header-btn ec-sidebar-toggle">
+                            <i class="fi fi-rr-apps"></i>
+                        </a>
+                        <!-- Header menu Start -->
+                        <a href="#ec-mobile-menu" class="ec-header-btn ec-side-toggle d-lg-none">
+                            <i class="fi fi-rr-menu-burger"></i>
+                        </a>
+                        <!-- Header menu End -->
+                    </div>
+                </div>
+                <!-- Header Top responsive Action -->
+            </div>
+        </div>
+    </div>
     <!-- Ec Header Top  End -->
     <!-- Ec Header Bottom  Start -->
     <div class="ec-header-bottom d-none d-lg-block">
@@ -42,7 +84,8 @@
                                 <ul class="dropdown-menu dropdown-menu-right">
                                     @if (auth()->check())
                                         @if (auth()->user()->isAdmin())
-                                            <li><a class="dropdown-item" href="{{ route('admin.home') }}">Dashboard</a>
+                                            <li><a class="dropdown-item"
+                                                    href="{{ route('admin.home') }}">Dashboard</a>
                                             </li>
                                         @endif
                                         <li><a class="dropdown-item" href="{{ route('logout') }}">Logout</a></li>
@@ -103,7 +146,8 @@
                 <div class="col">
                     <div class="header-search">
                         <form class="ec-btn-group-form" action="#">
-                            <input class="form-control ec-search-bar" placeholder="Search products..." type="text">
+                            <input class="form-control ec-search-bar" placeholder="Search products..."
+                                type="text">
                             <button class="submit" type="submit"><i class="fi-rr-search"></i></button>
                         </form>
                     </div>
